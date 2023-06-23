ecb05f93af65852cbc3040d45b535674aaa6ddd1
Andrei Piontkovskii <andrei@p10i.ru>
2023-06-23 11:22:43 +0000

    #55 replace "jump" to "vngn"

--- infrastructure/development/configuration/traefik/create-cert.sh
+++ infrastructure/development/configuration/traefik/create-cert.sh
@@ -2,8 +2,8 @@
 
 # Create new cert & key files
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
-  -keyout local.key -out local.cert -subj "/CN=application.jump" \
-  -addext "subjectAltName=DNS:*.application.jump"
+  -keyout local.key -out local.cert -subj "/CN=application.vngn" \
+  -addext "subjectAltName=DNS:*.application.vngn"
 
 # Create new pem file to import in the browser
 cat local.cert local.key > ../../../local.certificate.$(date +"%Y-%m-%d_%H-%M-%S").pem
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn how to jump
+          Learn how to vngn
   
         </a>
       </header>
